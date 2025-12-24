57db43a2d94496dce4ee2abef79ba84a0ee9d16b
Allwin Raja J <95063940+allwin107@users.noreply.github.com>
2025-12-24 11:58:44 +0530

    added download resume

--- components/Hero.js
+++ components/Hero.js
@@ -49,6 +49,12 @@ export default function Hero() {
                         </div>
 
                         <div className={styles.cta}>
+                            <a href="/resume/Allwin_Raja_J_Resume.pdf" download className="btn btn-primary">
+                                <svg width="20" height="20" viewBox="0 0 20 20" fill="currentColor" style={{ marginRight: '8px' }}>
+                                    <path fillRule="evenodd" d="M3 17a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm3.293-7.707a1 1 0 011.414 0L9 10.586V3a1 1 0 112 0v7.586l1.293-1.293a1 1 0 111.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z" clipRule="evenodd" />
+                                </svg>
+                                Download Resume
+                            </a>
                             <a href="#projects" className="btn btn-primary">View Projects</a>
                             <a href="#contact" className="btn btn-outline">Contact Me</a>
                         </div>
---
+++ public/resume/README.md
@@ -0,0 +1,22 @@
+# Resume Directory
+
+## Instructions
+
+Place your PDF resume in this directory with the following name:
+
+**`Allwin_Raja_J_Resume.pdf`**
+
+This file will be downloadable from the "Download Resume" button on your portfolio's home page.
+
+## Important Notes
+
+- The file name must match exactly: `Allwin_Raja_J_Resume.pdf`
+- Make sure it's a PDF file
+- Keep the file size reasonable (under 2MB is recommended)
+- Update the resume regularly to keep it current
+
+## Current Status
+
+- [ ] Resume PDF uploaded
+
+Once you add your resume PDF here, the download button will work automatically!
